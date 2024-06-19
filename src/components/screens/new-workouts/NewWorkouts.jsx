25e0f2d59b5985bc315520dc32b6f983fc111713
Maryess Mary <vip.ivan.ivanov2003@bk.ru>
2024-06-19 16:36:53 +0300

    изменено: использование готового компонента react select(буз стилей)

--- src/components/screens/new-workouts/NewWorkouts.jsx
+++ src/components/screens/new-workouts/NewWorkouts.jsx
@@ -5,15 +5,16 @@ import Button from '../../ui/button/Button'
 
 import { useMutation } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
-import workoutService from '../../../services/workoutService'
+import WorkoutService from '../../../services/workoutService'
 import Header from '../../layout/header/Header'
+import Loading from '../../ui/Loading'
 import Field from '../../ui/field/Field'
 import ExerciseList from './ExerciseList'
 import styles from './NewWorkouts.module.scss'
 // const data = ['chest', 'shoulders', 'biceps', 'legs', 'hit', 'back']
 
 const NewWorkout = () => {
-	const { register, handleSubmit, reset } = useForm({
+	const { control, register, handleSubmit, reset } = useForm({
 		mode: 'onChange'
 	})
 	// const {} = useFieldArray(
@@ -24,21 +25,25 @@ const NewWorkout = () => {
 	// 	}
 	// )
 
-	const { mutate } = useMutation(
-		['create exercise'],
-		body => workoutService.create(body),
+	const { mutate, isLoading } = useMutation(
+		['create workout'],
+		body => WorkoutService.create(body),
 		{
 			onSuccess: () => {
-				alert('success')
-				reset()
+				alert('Workouts created')
+				reset({
+					name: '',
+					exerciseIds: []
+				})
 			}
 		}
 	)
 
-	const onSubmit = async data => {
-		mutate(data)
-		console.log(data)
-		alert(data)
+	const onSubmit = data => {
+		mutate({
+			name: data.name,
+			exerciseIds: data.exerciseIds.map(ex => ex.value)
+		})
 	}
 
 	return (
@@ -56,6 +61,7 @@ const NewWorkout = () => {
 					<h1 className={stylesLayout.heading}>Create new workout</h1>
 				</div>
 			</div>
+			{isLoading ? <Loading /> : null}
 			<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
 				<Field
 					name={'name'}
@@ -65,7 +71,7 @@ const NewWorkout = () => {
 					options={{ required: 'Exercise is required' }}
 				/>
 
-				<ExerciseList />
+				<ExerciseList control={control} />
 
 				<Button heading='Create' />
 			</form>
